bc5ff3b720c117c4200bb14c44365f57fa8cf232
soheib kiani <mrsoheibkiani@gmail.com>
2023-02-04 21:23:33 +0330

    Resposive P-1 Of Header ( sm-md-lg)

--- components/Layout.js
+++ components/Layout.js
@@ -14,30 +14,30 @@ export default function Layout({ title, children }) {
       <div className="flex min-h-screen flex-col justify-between ">
         <header>
           {/* Header */}
-          <nav className="flex h-24 border-b items-center px-16  justify-between ">
+          <nav className="flex gap-x-6 lg:gap-x-0  h-24 border-b items-center px-8 lg:px-16  justify-between ">
             {/* Header Icons */}
-            <div className="flex gap-x-4 ">
-              <div className="flex p-1 justify-center items-center border border-gray-700 rounded-lg">
-                <img className="w-6 h-6" src="/icon/buy.png" alt="but-logo" />
-                <Link className="p-1 " href="/">
+            <div className="flex gap-x-1 md:gap-x-3 lg:gap-x-4 ">
+              <div className="flex justify-center text-center items-center border border-gray-700 rounded-lg lg:p-1 hover:font-semibold ">
+                <img className="w-6 h-6" src="/icon/buy.png" alt="buy-logo" />
+                <Link className=" text-[10px] w-16 md:p-2 lg:text-sm lg:p-1 " href="/">
                   سبد خرید
                 </Link>
               </div>
-              <div className="flex  justify-center items-center border border-gray-700 rounded-lg">
+              <div className="flex justify-center text-center items-center border border-gray-700 rounded-lg hover:font-semibold">
                 <img
                   className="w-6 h-6"
                   src="/icon/profile.png"
                   alt="but-logo"
                 />
-                <Link className="p-1 " href="/">
-                  ورود / ثبت نام
+                <Link className="text-[10px] w-20 lg:text-sm lg:p-1" href="/">
+                  ورود/ثبت نام
                 </Link>
               </div>
             </div>
             {/* Search Box */}
-            <div className="flex gap-x-16 ">
+            <div className="flex lg:gap-x-16 ">
               <div className="relative">
-                <div className="relative flex justify-center items-center w-full">
+                <div className="relative flex  justify-center items-center w-full">
                   <div className="absolute  right-2  items-center  pointer-events-none">
                     <img
                       src="icon/search.png"
@@ -50,7 +50,8 @@ export default function Layout({ title, children }) {
                       dir="rtl"
                       id=""
                       type="text"
-                      className="placeholder-gray-600 pb-2 pr-7 bg-gray-200 border border-gray-300 text-gray-900 text-base rounded-lg   w-full pl-28 p-1.5 "
+                      className="placeholder-gray-600 pb-1 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg   w-full pr-6 md:pl-24 lg:pr-7 lg:pl-28 lg:p-1.5 lg:pb-2 lg:text-base "
+                      // className="placeholder-gray-600 pb-2 pr-7 bg-gray-200 border border-gray-300 text-gray-900 text-base rounded-lg   w-full lg:pl-28 p-1 lg:p-1.5 "
                       placeholder="جست و جو"
                       required
                     />
@@ -58,16 +59,67 @@ export default function Layout({ title, children }) {
                 </div>
               </div>
               {/* Logo Icon */}
-              <div>
+              <div className="hidden lg:block">
                 <Link className="text-lg font-bold" href="/">
                   icon
                 </Link>
               </div>
             </div>
           </nav>
-          
-          <nav className='border-b'>
-            part 2
+          {/* Filter Header */}
+          <nav className=" h-9 flex items-center  border-b text-sm">
+            <div className="flex w-full justify-between items-center px-64">
+              <div className="w-auto gap-x-1 flex cursor-pointer ">
+                <div>ثبت فروشگاه</div>
+                <div className="">
+                  <img
+                    src="/icon/store.png"
+                    alt="store-icon"
+                    className="w-6 h-6"
+                  />
+                </div>
+              </div>
+              <div className="w-auto gap-x-1 flex cursor-pointer">
+                <div>پیشنهادی ها</div>
+                <div>
+                  <img
+                    src="/icon/suggest.png"
+                    alt="store-icon"
+                    className="w-6 h-6"
+                  />
+                </div>
+              </div>
+              <div className="w-auto gap-x-1 flex cursor-pointer">
+                <div>تخفیفات ویژه</div>
+                <div>
+                  <img
+                    src="/icon/discount.png"
+                    alt="store-icon"
+                    className="w-6 h-6"
+                  />
+                </div>
+              </div>
+              <div className="w-auto gap-x-1 flex cursor-pointer">
+                <div>پرفروش ها</div>
+                <div>
+                  <img
+                    src="/icon/fire.png"
+                    alt="store-icon"
+                    className="w-6 h-6"
+                  />
+                </div>
+              </div>
+              <div className="w-auto gap-x-1 flex cursor-pointer">
+                <div>دسته بندی کالا ها</div>
+                <div>
+                  <img
+                    src="/icon/list.png"
+                    alt="store-icon"
+                    className="w-6 h-6"
+                  />
+                </div>
+              </div>
+            </div>
           </nav>
         </header>
         {/* Body */}
