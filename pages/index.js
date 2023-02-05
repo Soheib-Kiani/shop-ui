3f4c91f237e7e8475720834e05fad5d19a9b7cd1
soheib kiani <mrsoheibkiani@gmail.com>
2023-02-05 22:00:34 +0330

    Create Category Boxes (Just Grid and Place Them Center)

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 // # COMPONENTS
 import Layout from '@/components/Layout';
 import Carousel from '@/components/carousel';
+import Category from '@/components/category';
 
 export default function Home() {
 
   return (
     <Layout title="Home">
       <Carousel />
+      <Category />
     </Layout>
   );
 }
