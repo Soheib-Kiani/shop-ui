527751db02d7ffc34a9a6e1ca2bd225a4e19c235
soheib kiani <mrsoheibkiani@gmail.com>
2023-02-06 21:51:09 +0330

    Responsive Category(sm-md-lg)

--- components/category.js
+++ components/category.js
@@ -1,21 +1,69 @@
+import Image from 'next/image';
+
+/* eslint-disable @next/next/no-img-element */
 const category = () => {
   return (
-    <section className="grid grid-cols-4 place-items-center max-w-7xl mt-1 m-auto">
-      <div className="w-24 h-24 shadow-lg text-center rounded-lg bg-red-500">
-        <div className="w-full h-3/4 rounded-t-lg bg-yellow-400">img</div>
-        <div>text</div>
+    <section className="grid grid-cols-5 place-items-center max-w-7xl mt-1 m-auto">
+      <div className="w-14 h-1w-14 md:w-24 md:h-24  text-center rounded-lg ">
+        <div className="w-full h-3/4 rounded-t-lg ">
+          <Image
+            width={100}
+            height={100}
+            priority={true}
+            alt="category-picture"
+            src={'/photo/cat1.png'}
+          />
+          {/* When true, the image will be considered high priority and preload. Lazy loading is automatically disabled for images using priority. */}
+        </div>
+        <div>cat1</div>
       </div>
-      <div className="w-24 h-24 rounded-lg text-center bg-green-500">
-        <div className="w-full h-3/4 rounded-t-lg bg-yellow-400">img</div>
-        <div>text</div>
+      <div className="w-14 h-1w-14 md:w-24 md:h-24  rounded-lg text-center ">
+        <div className="w-full h-3/4 rounded-t-lg ">
+          <Image
+            width={100}
+            height={100}
+            priority={true}
+            alt="category-picture"
+            src={'/photo/cat1.png'}
+          />
+        </div>
+        <div>cat2</div>
       </div>
-      <div className="w-24 h-24 rounded-lg text-center bg-yellow-500">
-        <div className="w-full h-3/4 rounded-t-lg bg-yellow-400">img</div>
-        <div>text</div>
+      <div className="w-14 h-1w-14 md:w-24 md:h-24  rounded-lg text-center ">
+        <div className="w-full h-3/4 rounded-t-lg ">
+          <Image
+            width={100}
+            height={100}
+            priority={true}
+            alt="category-picture"
+            src={'/photo/cat3.png'}
+          />
+        </div>
+        <div>cat 3</div>
       </div>
-      <div className="w-24 h-24 rounded-lg text-center bg-gray-500">
-        <div className="w-full h-3/4 rounded-t-lg bg-yellow-400">img</div>
-        <div>text</div>
+      <div className="w-14 h-1w-14 md:w-24 md:h-24  rounded-lg text-center ">
+        <div className="w-full h-3/4 rounded-t-lg ">
+          <Image
+            width={100}
+            height={100}
+            priority={true}
+            alt="category-picture"
+            src={'/photo/cat5.png'}
+          />
+        </div>
+        <div>cat 4</div>
+      </div>
+      <div className="w-14 h-1w-14 md:w-24 md:h-24  rounded-lg text-center ">
+        <div className="w-full h-3/4 rounded-t-lg ">
+          <Image
+            width={100}
+            height={100}
+            priority={true}
+            alt="category-picture"
+            src={'/photo/cat5.png'}
+          />
+        </div>
+        <div>cat 5</div>
       </div>
     </section>
   );
