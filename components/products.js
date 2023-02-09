389b3fc7382e1ade2d8468d2bc6128c64a076690
soheib kiani <mrsoheibkiani@gmail.com>
2023-02-09 22:11:32 +0330

    Finish Brand Component

--- components/products.js
+++ components/products.js
@@ -4,11 +4,11 @@ const products = () => {
   return (
     <section className="max-w-sm mt-3 md:max-w-2xl lg:max-w-7xl m-auto">
       <div className="grid grid-cols-3 lg:grid-cols-4  md:place-items-center lg:place-items-center  gap-6 ">
-        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
-            %200
+        <div className="h-auto w-auto md:w-44 lg:w-56   bg-gray-100 shadow rounded-md ">
+          <div className="m-2 text-center bg-red-500 text-sm  rounded-md h-auto w-11 ">
+            -20%
           </div>
-          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+          <div className="p-1 md:p-2  md:grid md:place-items-center">
             <img
               className="h-full w-full"
               loading="lazy"
@@ -16,29 +16,29 @@ const products = () => {
               src="/photo/product1.png"
             />
           </div>
-          <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center md:pb-1">Iphone 5</p>
+          <p className="grid place-items-center font-medium">15000$</p>
+          <p className="grid place-items-center font-bold md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
-            %200
+        <div className="h-auto w-auto md:w-44 lg:w-56   bg-gray-100 shadow rounded-md ">
+          <div className="m-2 text-center bg-red-500 text-sm  rounded-md h-auto w-11 ">
+            -30%
           </div>
-          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+          <div className="p-1 md:p-2  md:grid md:place-items-center">
             <img
               className="h-full w-full"
               loading="lazy"
               alt="category-picture"
-              src="/photo/product1.png"
+              src="/photo/product2.png"
             />
           </div>
-          <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center md:pb-1">Iphone 5</p>
+          <p className="grid place-items-center font-medium">13500</p>
+          <p className="grid place-items-center font-bold md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
-            %200
+        <div className="h-auto w-auto md:w-44 lg:w-56   bg-gray-100 shadow rounded-md ">
+          <div className="m-2 text-center bg-red-500 text-sm  rounded-md h-auto w-11 ">
+            -10%
           </div>
-          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+          <div className="p-1 md:p-2  md:grid md:place-items-center">
             <img
               className="h-full w-full"
               loading="lazy"
@@ -46,44 +46,44 @@ const products = () => {
               src="/photo/product1.png"
             />
           </div>
-          <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center md:pb-1">Iphone 5</p>
+          <p className="grid place-items-center font-medium">54000</p>
+          <p className="grid place-items-center font-bold md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
-            %200
+        <div className="h-auto w-auto md:w-44 lg:w-56   bg-gray-100 shadow rounded-md ">
+          <div className="m-2 text-center bg-red-500 text-sm  rounded-md h-auto w-11 ">
+            -40%
           </div>
-          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+          <div className="p-1 md:p-2  md:grid md:place-items-center">
             <img
               className="h-full w-full"
               loading="lazy"
               alt="category-picture"
-              src="/photo/product1.png"
+              src="/photo/product2.png"
             />
           </div>
-          <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center md:pb-1">Iphone 5</p>
+          <p className="grid place-items-center font-medium">78000</p>
+          <p className="grid place-items-center font-bold md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
-            %200
+        <div className="h-auto w-auto md:w-44 lg:w-56   bg-gray-100 shadow rounded-md ">
+          <div className="m-2 text-center bg-red-500 text-sm  rounded-md h-auto w-11 ">
+            -30%
           </div>
-          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+          <div className="p-1 md:p-2  md:grid md:place-items-center">
             <img
               className="h-full w-full"
               loading="lazy"
               alt="category-picture"
-              src="/photo/product1.png"
+              src="/photo/product2.png"
             />
           </div>
-          <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center md:pb-1">Iphone 5</p>
+          <p className="grid place-items-center font-medium">60000</p>
+          <p className="grid place-items-center font-bold md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
-            %200
+        <div className="h-auto w-auto md:w-44 lg:w-56   bg-gray-100 shadow rounded-md ">
+          <div className="m-2 text-center bg-red-500 text-sm  rounded-md h-auto w-11 ">
+            -20%
           </div>
-          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+          <div className="p-1 md:p-2  md:grid md:place-items-center">
             <img
               className="h-full w-full"
               loading="lazy"
@@ -91,29 +91,29 @@ const products = () => {
               src="/photo/product1.png"
             />
           </div>
-          <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center md:pb-1">Iphone 5</p>
+          <p className="grid place-items-center font-medium">64580</p>
+          <p className="grid place-items-center font-bold md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
-            %200
+        <div className="h-auto w-auto md:w-44 lg:w-56   bg-gray-100 shadow rounded-md ">
+          <div className="m-2 text-center bg-red-500 text-sm  rounded-md h-auto w-11 ">
+            -25%
           </div>
-          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+          <div className="p-1 md:p-2  md:grid md:place-items-center">
             <img
               className="h-full w-full"
               loading="lazy"
               alt="category-picture"
-              src="/photo/product1.png"
+              src="/photo/product2.png"
             />
           </div>
-          <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center md:pb-1">Iphone 5</p>
+          <p className="grid place-items-center font-medium">45000</p>
+          <p className="grid place-items-center font-bold md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
-            %200
+        <div className="h-auto w-auto md:w-44 lg:w-56   bg-gray-100 shadow rounded-md ">
+        <div className="m-2 text-center bg-red-500 text-sm  rounded-md h-auto w-11 ">
+            -13%
           </div>
-          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+          <div className="p-1 md:p-2 md:grid md:place-items-center">
             <img
               className="h-full w-full"
               loading="lazy"
@@ -121,8 +121,8 @@ const products = () => {
               src="/photo/product1.png"
             />
           </div>
-          <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center md:pb-1">Iphone 5</p>
+          <p className="grid place-items-center font-medium">12000</p>
+          <p className="grid place-items-center font-bold md:pb-1">Iphone 5</p>
         </div>
       </div>
     </section>
