b87cb38b001488c222ae579eee3a94f8812a255b
soheib kiani <mrsoheibkiani@gmail.com>
2023-02-09 21:43:38 +0330

    Responsive Brand(sm-md-lg)

--- components/products.js
+++ components/products.js
@@ -1,104 +1,128 @@
-import Image from 'next/image';
+/* eslint-disable @next/next/no-img-element */
 
 const products = () => {
   return (
     <section className="max-w-sm mt-3 md:max-w-2xl lg:max-w-7xl m-auto">
-      <div className="grid grid-cols-3 md:place-items-center  gap-6 ">
-        <div className="h-auto w-auto md:w-44   bg-gray-100 shadow rounded-md ">
+      <div className="grid grid-cols-3 lg:grid-cols-4  md:place-items-center lg:place-items-center  gap-6 ">
+        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
           <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
             %200
           </div>
-          <div className='p-2 md:p-4 md:grid md:place-items-center'>
-            <Image
-              width={100}
-              height={100}
-              priority={false}
+          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+            <img
+              className="h-full w-full"
+              loading="lazy"
               alt="category-picture"
-              src={'/photo/product1.png'}
+              src="/photo/product1.png"
             />
           </div>
           <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center">Iphone 5</p>
+          <p className="grid place-items-center md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 p-2 md:p-4  bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
+        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
+          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
             %200
           </div>
-          <div className='md:grid md:place-items-center'>
-            <Image
-              width={100}
-              height={100}
-              priority={false}
+          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+            <img
+              className="h-full w-full"
+              loading="lazy"
               alt="category-picture"
-              src={'/photo/product2.png'}
+              src="/photo/product1.png"
             />
           </div>
           <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center">Iphone 5</p>
+          <p className="grid place-items-center md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 p-2 md:p-4  bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
+        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
+          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
             %200
           </div>
-          <div className='md:grid md:place-items-center'>
-            <Image
-              width={100}
-              height={100}
-              priority={false}
+          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+            <img
+              className="h-full w-full"
+              loading="lazy"
               alt="category-picture"
-              src={'/photo/product1.png'}
+              src="/photo/product1.png"
             />
           </div>
           <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center">Iphone 5</p>
+          <p className="grid place-items-center md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 p-2 md:p-4  bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
+        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
+          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
             %200
           </div>
-          <div className='md:grid md:place-items-center'>
-            <Image
-              width={100}
-              height={100}
-              priority={false}
+          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+            <img
+              className="h-full w-full"
+              loading="lazy"
               alt="category-picture"
-              src={'/photo/product2.png'}
+              src="/photo/product1.png"
             />
           </div>
           <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center">Iphone 5</p>
+          <p className="grid place-items-center md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 p-2 md:p-4  bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
+        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
+          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
             %200
           </div>
-          <div className='md:grid md:place-items-center'>
-            <Image
-              width={100}
-              height={100}
-              priority={false}
+          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+            <img
+              className="h-full w-full"
+              loading="lazy"
               alt="category-picture"
-              src={'/photo/product1.png'}
+              src="/photo/product1.png"
             />
           </div>
           <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center">Iphone 5</p>
+          <p className="grid place-items-center md:pb-1">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto md:w-44 p-2 md:p-4  bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
+        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
+          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
             %200
           </div>
-          <div className='md:grid md:place-items-center'>
-            <Image
-              width={100}
-              height={100}
-              priority={false}
+          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+            <img
+              className="h-full w-full"
+              loading="lazy"
               alt="category-picture"
-              src={'/photo/product2.png'}
+              src="/photo/product1.png"
             />
           </div>
           <p className="grid place-items-center">9586</p>
-          <p className="grid place-items-center">Iphone 5</p>
+          <p className="grid place-items-center md:pb-1">Iphone 5</p>
+        </div>
+        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
+          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
+            %200
+          </div>
+          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+            <img
+              className="h-full w-full"
+              loading="lazy"
+              alt="category-picture"
+              src="/photo/product1.png"
+            />
+          </div>
+          <p className="grid place-items-center">9586</p>
+          <p className="grid place-items-center md:pb-1">Iphone 5</p>
+        </div>
+        <div className="h-auto w-auto md:w-44 lg:w-52   bg-gray-100 shadow rounded-md ">
+          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
+            %200
+          </div>
+          <div className="p-1 md:p-2 lg:p-4 md:grid md:place-items-center">
+            <img
+              className="h-full w-full"
+              loading="lazy"
+              alt="category-picture"
+              src="/photo/product1.png"
+            />
+          </div>
+          <p className="grid place-items-center">9586</p>
+          <p className="grid place-items-center md:pb-1">Iphone 5</p>
         </div>
       </div>
     </section>
