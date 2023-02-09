b43c794c9645899a8def12c25e1046a7b94e9fa4
soheib kiani <mrsoheibkiani@gmail.com>
2023-02-09 20:32:29 +0330

    Create Brand Components Structure for md

--- components/products.js
+++ components/products.js
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 const products = () => {
   return (
-    <section className="max-w-sm mt-2 md:max-w-2xl lg:max-w-7xl m-auto">
-      <div className="grid grid-cols-3  gap-2 ">
-        <div className="h-auto w-auto bg-gray-100 shadow rounded-md ">
-          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
+    <section className="max-w-sm mt-3 md:max-w-2xl lg:max-w-7xl m-auto">
+      <div className="grid grid-cols-3 md:place-items-center  gap-6 ">
+        <div className="h-auto w-auto md:w-44   bg-gray-100 shadow rounded-md ">
+          <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11 ">
             %200
           </div>
-          <div>
+          <div className='p-2 md:p-4 md:grid md:place-items-center'>
             <Image
               width={100}
               height={100}
@@ -20,11 +20,11 @@ const products = () => {
           <p className="grid place-items-center">9586</p>
           <p className="grid place-items-center">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto  bg-gray-100 shadow rounded-md ">
+        <div className="h-auto w-auto md:w-44 p-2 md:p-4  bg-gray-100 shadow rounded-md ">
           <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
             %200
           </div>
-          <div>
+          <div className='md:grid md:place-items-center'>
             <Image
               width={100}
               height={100}
@@ -36,11 +36,11 @@ const products = () => {
           <p className="grid place-items-center">9586</p>
           <p className="grid place-items-center">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto  bg-gray-100 shadow rounded-md ">
+        <div className="h-auto w-auto md:w-44 p-2 md:p-4  bg-gray-100 shadow rounded-md ">
           <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
             %200
           </div>
-          <div>
+          <div className='md:grid md:place-items-center'>
             <Image
               width={100}
               height={100}
@@ -52,11 +52,11 @@ const products = () => {
           <p className="grid place-items-center">9586</p>
           <p className="grid place-items-center">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto  bg-gray-100 shadow rounded-md ">
+        <div className="h-auto w-auto md:w-44 p-2 md:p-4  bg-gray-100 shadow rounded-md ">
           <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
             %200
           </div>
-          <div>
+          <div className='md:grid md:place-items-center'>
             <Image
               width={100}
               height={100}
@@ -68,11 +68,11 @@ const products = () => {
           <p className="grid place-items-center">9586</p>
           <p className="grid place-items-center">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto  bg-gray-100 shadow rounded-md ">
+        <div className="h-auto w-auto md:w-44 p-2 md:p-4  bg-gray-100 shadow rounded-md ">
           <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
             %200
           </div>
-          <div>
+          <div className='md:grid md:place-items-center'>
             <Image
               width={100}
               height={100}
@@ -84,11 +84,11 @@ const products = () => {
           <p className="grid place-items-center">9586</p>
           <p className="grid place-items-center">Iphone 5</p>
         </div>
-        <div className="h-auto w-auto  bg-gray-100 shadow rounded-md ">
+        <div className="h-auto w-auto md:w-44 p-2 md:p-4  bg-gray-100 shadow rounded-md ">
           <div className="flex justify-start items-start bg-red-600 text-sm text-center rounded-md h-auto w-11">
             %200
           </div>
-          <div>
+          <div className='md:grid md:place-items-center'>
             <Image
               width={100}
               height={100}
